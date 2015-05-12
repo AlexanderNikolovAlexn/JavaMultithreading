72bec3803f62583f22c0d58e551fc4f638aa57cc
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-05-12 15:27:26 +0300

    avoiding deadlocks with tryLock method

---
+++ src/demo13Deadlock/App.java
@@ -0,0 +1,50 @@
+package demo13Deadlock;
+
+public class App {
+
+	public static void main(String[] args) {
+		Runner runner = new Runner();
+
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					runner.firstThread();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					runner.secondThread();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		runner.finished();
+	}
+
+}
